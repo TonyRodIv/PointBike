6a0c6d7bc57749c7bf9c3e155ccc9a28fe0ae87c
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-04-02 14:59:40 -0300

    Correção de bugs e Melhorias!!
    -Correção de bugs relacionados ao usuário;
    -Melhorias na página inicial ao estar logado.

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 let userE = localStorage.getItem('userD');
 let semUsuario = document.getElementById('menu')
 let comUsuario = document.getElementById('userInfo')
+let btnCadastro = document.getElementById('cadastroBtn')
+let btnMapa = document.getElementById('mapaBtn')
 console.log(userE)
 if(userE === null){
-    semUsuario.style.display = 'none'
-    comUsuario.style.display = 'flex'
+    semUsuario.style.display = 'flex'
+    comUsuario.style.display = 'none'
+    btnCadastro.style.display = 'flex'
+    btnMapa.style.display = 'none'
+    console.log('você NÃO está logado')
 }else{
     semUsuario.style.display = 'none'
+    btnCadastro.style.display = 'none'
+    btnMapa.style.display = 'flex'
     comUsuario.style.display = 'flex'
+    console.log('você está logado')
     document.getElementById('username').innerHTML = userE
 }
